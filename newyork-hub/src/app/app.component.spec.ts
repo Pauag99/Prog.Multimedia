6458702487529eb1b180c554526921bf26958a80
Pau Aguilar Silvestre <78934508+Pauag99@users.noreply.github.com>
2025-02-18 20:05:32 +0100

    V0.1

--- newyork-hub/src/app/app.component.spec.ts
+++ newyork-hub/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Multimedia' title`, () => {
+  it(`should have the 'newyork-hub' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Multimedia');
+    expect(app.title).toEqual('newyork-hub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Multimedia');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, newyork-hub');
   });
 });
