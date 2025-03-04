1377d8d5c3f2308311acac0beeecc327f5414bc8
Pau Aguilar Silvestre <78934508+Pauag99@users.noreply.github.com>
2025-03-04 19:07:16 +0100

    Generando build en la carpeta correcta

---
+++ docs/browser/chunk-LAZ36WJI.js
@@ -0,0 +1,2 @@
+import{Ia as n,ta as t}from"./chunk-IKPA6MXN.js";var p=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-musicpage"]],decls:1,vars:0,template:function(e,m){e&1&&n(0,`hola
+`)},encapsulation:2})};export{p as MusicpageComponent};
