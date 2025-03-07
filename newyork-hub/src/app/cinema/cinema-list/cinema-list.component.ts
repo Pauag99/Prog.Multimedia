5f70b96f8324dec5c84a468d5ffb602211b471a4
AJLMT <ajlmthosab30@yahoo.es>
2025-03-07 10:52:28 +0100

    music implemented

--- newyork-hub/src/app/cinema/cinema-list/cinema-list.component.ts
+++ newyork-hub/src/app/cinema/cinema-list/cinema-list.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-cinema-list',
-  imports: [],
   templateUrl: './cinema-list.component.html',
-  styleUrl: './cinema-list.component.scss'
+  styleUrls: ['./cinema-list.component.scss']
 })
-export class CinemaListComponent {
+export class CinemaListComponent implements AfterViewInit {
+  @ViewChild('video') videoElement!: ElementRef<HTMLVideoElement>;
+  duration: string = '0';
+  currentTime: string = '0';
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.videoElement.nativeElement.addEventListener('loadedmetadata', () => {
+      this.duration = this.videoElement.nativeElement.duration.toFixed(0);
+      this.cdr.detectChanges();
+    });
+
+    this.videoElement.nativeElement.addEventListener('timeupdate', () => {
+      this.currentTime = this.videoElement.nativeElement.currentTime.toFixed(0);
+      this.cdr.detectChanges();
+    });
+  }
+
+  playVideo() {
+    this.videoElement.nativeElement.play();
+  }
+
+  pauseVideo() {
+    this.videoElement.nativeElement.pause();
+  }
+
+  stopVideo() {
+    this.videoElement.nativeElement.pause();
+    this.resetVideo()
+  }
+
+  fastForward() {
+    this.videoElement.nativeElement.currentTime += 10;
+  }
+
+  changeVolume(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.videoElement.nativeElement.volume = parseFloat(input.value);
+  }
+
+  toggleFullScreen() {
+    if (this.videoElement.nativeElement.requestFullscreen) {
+      this.videoElement.nativeElement.requestFullscreen();
+    }
+  }
+
+  resetVideo() {
+    this.videoElement.nativeElement.currentTime = 0;
+    this.currentTime = this.videoElement.nativeElement.currentTime.toFixed(0);
+    this.cdr.detectChanges();
+  }
+
+  seekVideo(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.videoElement.nativeElement.currentTime = parseFloat(input.value);
+    this.currentTime = this.videoElement.nativeElement.currentTime.toFixed(0);
+    this.cdr.detectChanges();
+  }
 }
