4a6320dd4a47d47fc02d2f2f61c463406ec7258f
Pau Aguilar Silvestre <78934508+Pauag99@users.noreply.github.com>
2025-03-10 16:21:44 +0100

    cambio mapa

---
+++ newyork-hub/src/app/sports/map/map.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements OnInit {
+  private map: any;
+
+  ngOnInit(): void {
+    this.initMap();
+    this.getUserLocation();
+  }
+
+  // Método para inicializar el mapa en una ubicación por defecto
+  private initMap(): void {
+    this.map = L.map('map').setView([39.571465, -0.517826], 15); // Ubicación predeterminada
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
+
+  // Método para obtener la ubicación del usuario
+  private getUserLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        this.map.setView([latitude, longitude], 15);
+
+        L.marker([latitude, longitude])
+          .addTo(this.map)
+          .bindPopup('Tu ubicación actual')
+          .openPopup();
+      }, () => {
+        console.error('No se pudo obtener la ubicación del usuario.');
+      });
+    } else {
+      console.error('La geolocalización no es compatible con este navegador.');
+    }
+  }
+}
--- newyork-hub/src/app/sports/sportspage/sportspage.component.ts
+++ newyork-hub/src/app/sports/sportspage/sportspage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SportsListComponent } from "../sports-list/sports-list.component";
+import { MapComponent } from '../map/map.component';
 
 @Component({
   selector: 'app-sportspage',
-  imports: [SportsListComponent],
+  imports: [SportsListComponent, MapComponent],
   templateUrl: './sportspage.component.html',
   styleUrl: './sportspage.component.scss'
 })
