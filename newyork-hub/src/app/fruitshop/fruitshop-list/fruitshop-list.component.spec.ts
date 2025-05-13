fcc9fe1f34cad21ed977c1a64df03b9154892d92
”anlomar8” <anlomar8@alumni.uv.es>
2025-05-13 16:26:10 +0200

    prueba

--- newyork-hub/src/app/fruitshop/fruitshop-list/fruitshop-list.component.spec.ts
+++ newyork-hub/src/app/fruitshop/fruitshop-list/fruitshop-list.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FruitshopListComponent } from './fruitshop-list.component';
 
 describe('FruitshopListComponent', () => {
-  let component: FruitshopListComponent;
+  let component: FruitshopListComponent; 
   let fixture: ComponentFixture<FruitshopListComponent>;
 
   beforeEach(async () => {
